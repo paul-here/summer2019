a50d9734229c26d8836cc643434f59a2dd816ac0
Paul Whipp <pwhipp@csumb.edu>
2019-05-20 19:03:52 -0700

    10 min mark

---
+++ shoppinglist/main.js
@@ -0,0 +1,22 @@
+// Paul Whipp
+// 5/20/2019
+// Shopping List Desktop App
+// https://www.youtube.com/watch?v=kN1Czs0m1SU
+// Following Tuturial by Traversy
+
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const {app, BrowserWindow} = electron;
+
+let mainWindow;
+
+// Listen for the app to be ready
+app.on('ready', function(){
+    // Create new window
+    mainWindow = new BrowserWindow({});
+    // Load html into window
+    mainWindow.loadURL(url.format('mainWindow.html'));
+
+}); // app ready
